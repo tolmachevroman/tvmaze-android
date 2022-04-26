e416c7a2d6643ae6fa5ab689563f1bc61399ca39
Roman Tolmachev <tolmachevroman@gmail.com>
2022-04-25 23:15:36 -0500

    refactor SubtopicView to a separate file

---
+++ app/src/main/java/com/tv/maze/widgets/SubtopicView.kt
@@ -0,0 +1,35 @@
+package com.tv.maze.widgets
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SubtopicView(title: String, content: String) {
+    Column {
+        Text(
+            text = "$title:",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(start = 16.dp, top = 4.dp)
+                .fillMaxWidth(),
+            color = Color.Black,
+        )
+        Text(
+            text = content,
+            fontSize = 18.sp,
+            modifier = Modifier
+                .padding(start = 20.dp)
+                .fillMaxWidth(),
+            color = Color.Black,
+        )
+    }
+}
