0fea9860bdb7b05f4f86a9c7fc87d14af2040e87
Roman Tolmachev <tolmachevroman@gmail.com>
2022-04-25 22:56:01 -0500

    add SeasonView, EpisodeView and some mock data to show seasons list

--- app/src/main/java/com/tv/maze/MainActivity.kt
+++ app/src/main/java/com/tv/maze/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.tv.maze
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import com.tv.maze.models.Episode
+import com.tv.maze.models.Poster
+import com.tv.maze.models.Season
 import com.tv.maze.ui.theme.TVmazeTheme
 import com.tv.maze.widgets.ShowDetailsScreen
 
@@ -20,13 +24,88 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val seasons = arrayListOf(
+                        Season(
+                            id = 1,
+                            number = 1,
+                            episodes = arrayListOf(
+                                Episode(
+                                    id = 1,
+                                    name = "The National Anthem",
+                                    season = 1,
+                                ),
+                                Episode(
+                                    id = 2,
+                                    name = "Fifteen Million Merits",
+                                    season = 1,
+                                ),
+                                Episode(
+                                    id = 3,
+                                    name = "The Entire History of You",
+                                    season = 1,
+                                )
+                            )
+                        ),
+                        Season(
+                            id = 2,
+                            number = 2,
+                            episodes = arrayListOf(
+                                Episode(
+                                    id = 4,
+                                    name = "Be Right Back",
+                                    season = 2,
+                                ),
+                                Episode(
+                                    id = 5,
+                                    name = "White Bear",
+                                    season = 2,
+                                ),
+                                Episode(
+                                    id = 6,
+                                    name = "The Waldo Moment",
+                                    season = 2,
+                                )
+                            )
+                        ),
+                        Season(
+                            id = 3,
+                            number = 3,
+                            episodes = arrayListOf(
+                                Episode(
+                                    id = 7,
+                                    name = "Nosedive",
+                                    season = 3,
+                                ),
+                                Episode(
+                                    id = 8,
+                                    name = "Playtest",
+                                    season = 3,
+                                ),
+                                Episode(
+                                    id = 9,
+                                    name = "Shut Up and Dance",
+                                    season = 3,
+                                ),
+                                Episode(
+                                    id = 10,
+                                    name = "San Junipero",
+                                    season = 3,
+                                )
+                            )
+                        )
+                    )
                     ShowDetailsScreen(
                         posterUrl = "https://as01.epimg.net/epik/imagenes/2018/03/13/portada/1520946522_348122_1520949182_noticia_normal.jpg",
                         name = "Black Mirror",
                         days = arrayListOf("Wednesday, Thursday"),
                         time = "22:00",
                         genres = arrayListOf("Drama, Thriller"),
-                        summary = "In an abstrusely dystopian future, several individuals grapple with the manipulative effects of cutting edge technology in their personal lives and behaviours."
+                        summary = "In an abstrusely dystopian future, several individuals grapple with the manipulative effects of cutting edge technology in their personal lives and behaviours.",
+                        seasons = seasons,
+                        onEpisodeClick = {
+                            Toast.makeText(this, "Clicked episode id $it", Toast.LENGTH_SHORT)
+                                .show()
+                        }
                     )
                 }
             }
--- app/src/main/java/com/tv/maze/widgets/ShowDetailsScreen.kt
+++ app/src/main/java/com/tv/maze/widgets/ShowDetailsScreen.kt
@@ -1,19 +1,28 @@
 package com.tv.maze.widgets
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import coil.size.Size
+import com.tv.maze.models.Episode
+import com.tv.maze.models.Poster
+import com.tv.maze.models.Season
 import com.tv.maze.ui.theme.TVmazeTheme
 
 @Composable
@@ -23,35 +32,60 @@ fun ShowDetailsScreen(
     days: ArrayList<String>,
     time: String,
     genres: ArrayList<String>,
-    summary: String
+    summary: String,
+    seasons: ArrayList<Season>,
+    onEpisodeClick: (Long) -> Unit
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize()
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(posterUrl)
-                .size(Size.ORIGINAL)
-                .build(),
-            contentScale = ContentScale.Crop,
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(220.dp)
-        )
-        Subtopic(title = "Name", content = name)
-        Subtopic(title = "Airing on", content = days.joinToString(separator = ",") + " at " + time)
-        Subtopic(title = "Genres", content = genres.joinToString(separator = ","))
-        Subtopic(
-            title = "Summary",
-            content = summary
-        )
-        //TODO list of episodes separated by season
+        item {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(posterUrl)
+                    .size(Size.ORIGINAL)
+                    .build(),
+                contentScale = ContentScale.Crop,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(220.dp)
+            )
+        }
+        item { SubtopicView(title = "Name", content = name) }
+        item {
+            SubtopicView(
+                title = "Airing on",
+                content = days.joinToString(separator = ",") + " at " + time
+            )
+        }
+        item { SubtopicView(title = "Genres", content = genres.joinToString(separator = ",")) }
+        item {
+            SubtopicView(
+                title = "Summary",
+                content = summary
+            )
+        }
+        item {
+            Text(
+                text = "Seasons:",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 10.dp)
+                    .fillMaxWidth(),
+                color = Color.Black,
+            )
+        }
+        seasons.forEach { season ->
+            item { SeasonView(season, onEpisodeClick) }
+        }
     }
 }
 
 @Composable
-fun Subtopic(title: String, content: String) {
+fun SubtopicView(title: String, content: String) {
     Column {
         Text(
             text = "$title:",
@@ -73,17 +107,98 @@ fun Subtopic(title: String, content: String) {
     }
 }
 
+@Composable
+fun SeasonView(season: Season, onEpisodeClick: (Long) -> Unit) {
+    Column {
+        Text(
+            text = "Season ${season.number}:",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(start = 16.dp, top = 4.dp)
+                .fillMaxWidth(),
+            color = Color.Black,
+        )
+        season.episodes.forEach { episode ->
+            EpisodeView(episode, onEpisodeClick)
+        }
+    }
+}
+
+@Composable
+fun EpisodeView(episode: Episode, onEpisodeClick: (Long) -> Unit) {
+    ClickableText(
+        text = AnnotatedString("- ${episode.name}"),
+        style = TextStyle(
+            fontSize = 18.sp,
+            textDecoration = TextDecoration.Underline
+        ),
+        onClick = {
+            onEpisodeClick(episode.id)
+        },
+        modifier = Modifier
+            .padding(start = 20.dp, end = 16.dp, top = 3.dp, bottom = 3.dp)
+            .fillMaxWidth()
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun ShowDetailsScreenPreview() {
     TVmazeTheme {
+        val seasons = arrayListOf(
+            Season(
+                id = 1,
+                number = 1,
+                episodes = arrayListOf(
+                    Episode(
+                        id = 1,
+                        name = "The National Anthem",
+                        season = 1,
+                    ),
+                    Episode(
+                        id = 2,
+                        name = "Fifteen Million Merits",
+                        season = 1,
+                    ),
+                    Episode(
+                        id = 3,
+                        name = "The Entire History of You",
+                        season = 1,
+                    )
+                )
+            ),
+            Season(
+                id = 2,
+                number = 2,
+                episodes = arrayListOf(
+                    Episode(
+                        id = 4,
+                        name = "Be Right Back",
+                        season = 2,
+                    ),
+                    Episode(
+                        id = 5,
+                        name = "White Bear",
+                        season = 2,
+                    ),
+                    Episode(
+                        id = 6,
+                        name = "The Waldo Moment",
+                        season = 2,
+                    )
+                )
+            )
+        )
         ShowDetailsScreen(
             posterUrl = "",
             name = "Black Mirror",
             days = arrayListOf("Wednesday, Thursday"),
             time = "22:00",
             genres = arrayListOf("Drama, Thriller"),
-            summary = "In an abstrusely dystopian future, several individuals grapple with the manipulative effects of cutting edge technology in their personal lives and behaviours."
+            summary = "In an abstrusely dystopian future, several individuals grapple with the manipulative effects of cutting edge technology in their personal lives and behaviours.",
+            seasons = seasons,
+            onEpisodeClick = { }
         )
     }
 }
